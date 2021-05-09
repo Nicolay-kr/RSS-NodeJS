852f39bce230d0bfc89fc3012ef6593b94cd7f03
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-05-09 23:56:51 +0300

    feat: add encoded mode

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,19 @@
 const chalk = require("chalk");
 const boxen = require("boxen");
-const alphabet =require("./alphabet.js")
+const alphabet = require("./alphabet.js");
+const fs = require("fs");
+const { Command } = require("commander");
+// const { readFile } = require("node:fs");
+
+const pathRead = "bin/input";
+const pathWrite = "bin/output";
+const option = {};
+
+const program = new Command();
+program.version("0.0.1");
+
+const write = fs.createWriteStream(pathWrite, option);
+const read = fs.createReadStream(pathRead, option);
 
 const greeting = chalk.white.bold("Hello!");
 
@@ -11,17 +24,56 @@ const boxenOptions = {
   borderColor: "green",
   backgroundColor: "#555555",
 };
-const msgBox = boxen(greeting, boxenOptions);
+// const msgBox = boxen(greeting, boxenOptions);
 
 // console.log(msgBox);
-
-
-fs = require("fs");
-fs.readFile("bin/input.txt", "utf8", function (err, data) {
-  if (err) {
-    return console.log(err);
+function caesarCipher(letter, shift) {
+  let index = alphabet.findIndex((item)=>item===letter);
+  let newIndex = index - shift;
+  if (newIndex < 0) {
+    newIndex = alphabet.length - shift;
   }
+  return alphabet[newIndex]
+}
 
-  console.log(data)
-});
-console.log(alphabet)
+function readMyFile(mode, file, shift) {
+  //   fs = require("fs");
+  fs.readFile(`bin/${file}`, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    const encodeArr = []
+    const arr = data.split("");
+    for (let i in arr) {
+      encodeArr.push(caesarCipher(arr[i],shift))
+
+    }
+
+    // return encodeArr.join("")
+    fs.writeFile('bin/output', encodeArr.join(""), function (err) {
+        if (err) return console.log(err);
+        console.log('message is encoded');
+      });
+
+    // console.log(arr);
+
+    // return data;
+  });
+}
+
+program
+  .requiredOption("-a, --action <type>", "an action encode/decode", "encode")
+  .requiredOption("-s, --shift <type>", "a shift")
+  .option("-i, --input <type>", "an input file")
+  .option("-o, --output", "an output file");
+
+program.parse();
+
+const options = program.opts();
+// console.log(program.opts().action)
+
+if (options.action) console.log(options.action);
+console.log("program start:");
+if (options.shift && options.shift)
+  readMyFile(options.action, options.input, options.shift);
+// if (options.action) console.log(`- ${options.pizzaType}`);
